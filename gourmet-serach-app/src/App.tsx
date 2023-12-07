2d26247a03bf2718748da39c72046feae9272b77
mabupro <mabupro787@gmail.com>
2023-12-07 13:33:31 +0900

    add/tailwind

--- gourmet-serach-app/src/App.tsx
+++ gourmet-serach-app/src/App.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '../public/vite.svg'
 import './App.css'
+import { Header } from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
