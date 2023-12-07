9444dc78d547fd2b8ba9aecbb0123c7917ca6e86
mabupro <mabupro787@gmail.com>
2023-12-07 18:39:18 +0900

    create/Header Design

--- gourmet-serach-app/src/components/Header.tsx
+++ gourmet-serach-app/src/components/Header.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import { Button } from "@material-tailwind/react";
+import React from 'react';
+import { Navbar, Button, Input } from "@material-tailwind/react";
 
 export const Header = () => {
     return (
-        <>
-            <Button>Header</Button>
-        </>
-    )
-}
+        <Navbar
+            variant="gradient"
+            color="orange"
+            className="mt-4 mx-auto md:m-5 max-w-screen-sm md:min-w-[480px] from-gray-100 to-gray-200 p-2 opacity-80"
+        >
+            <div className="flex w-full justify-start md:justify-center">
+                <div className="relative w-full">
+                    <Input
+                        type="search"
+                        color="orange"
+                        label="レストランを検索する..."
+                        className="w-full"
+                        containerProps={{
+                            className: "w-full",
+                        }} crossOrigin={undefined}
+                    />
+                    <Button
+                        size="sm"
+                        color="deep-orange"
+                        className="!absolute right-1 top-1 rounded"
+                    >
+                        検索
+                    </Button>
+                </div>
+            </div>
+        </Navbar>
+    );
+};
